f12c84aca4290e9d97003cbe4367bf3edbddb408
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-04-22 11:38:23 +0600

    hero section done

--- src/MainLayOut/MainLayOut.jsx
+++ src/MainLayOut/MainLayOut.jsx
@@ -7,7 +7,11 @@ const MainLayOut = () => {
     return (
         <>
           <Navbar></Navbar>  
-          <Outlet></Outlet>
+          <div className='min-h-[calc(100vh-116px)]'>
+                <div className=' mx-auto px-4 md:px-8 lg:px-12 xl:px-16'>
+                    <Outlet/>
+                </div>
+          </div>
           <Footer></Footer>
         </>
     );
