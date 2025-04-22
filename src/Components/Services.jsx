0006ebf0694f1422246d7fb9339df8e3231c117e
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-04-22 13:09:35 +0600

    service start

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import service1Img from '../assets/service-1.png'
 
 const Services = () => {
     return (
         <div>
-            services
+            <h2>We Provide Best Law Services</h2>
+            <p>Our platform connects you with verified, experienced Lawyers across various specialities — all at your convenience. </p>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
+                <div className='bg-gray-200 p-5 border rounded-2xl flex flex-col'>
+                    <img className='w-15' src={service1Img} alt="service-1 image" />
+                    <p>199+</p>
+                    <p>Total Lawyer</p>
+                </div>
+                <div className='bg-gray-200 p-5 border rounded-2xl flex flex-col'>
+                    <img className='w-10' src={service1Img} alt="service-1 image" />
+                    <p>199+</p>
+                    <p>Total Lawyer</p>
+                </div>
+                <div className='bg-gray-200 p-5 border rounded-2xl flex flex-col'>
+                    <img className='w-10' src={service1Img} alt="service-1 image" />
+                    <p>199+</p>
+                    <p>Total Lawyer</p>
+                </div>
+                <div className='bg-gray-200 p-5 border rounded-2xl flex flex-col'>
+                    <img className='w-10' src={service1Img} alt="service-1 image" />
+                    <p>199+</p>
+                    <p>Total Lawyer</p>
+                </div>
+            </div>
         </div>
     );
 };
