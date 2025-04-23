7da7a769e7c65bda9748b26e3eef0cabb3a41437
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-04-23 19:39:27 +0600

    error validation done when lawyer-details not available   in id

--- src/Components/BookingAppointment.jsx
+++ src/Components/BookingAppointment.jsx
@@ -5,21 +5,21 @@ const BookingAppointment = ({lawyer,handleRemoveBooking}) => {
     const {name, expert, visitPrice,} = lawyer;
     return (
         <div>
-                                    <div className='border border-gray-200 rounded-2xl p-10 mb-5'>
-                            <p className='font-bold text-xl mb-1'>{name}</p>
-                            <div className='flex justify-between'>
-                                <p className='text-base text-gray-400'>{expert}</p>
-                                <p className='text-base text-gray-400'>Appointment Fee: <span>{visitPrice} Taka</span></p>
-                            </div>
-                            <hr className='text-gray-500 border-dotted w-full my-3'/>
-                            <button 
-                                onClick={()=>{
-                                            handleRemoveBooking(lawyer)
-                                        }}
-                                href="#_" className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold leading-6 text-red-700 hover:text-white border border-red-600 rounded-full hover:bg-red-500 focus:outline-none focus:ring-red-600 cursor-pointer">
-                                Cancel Appointment
-                            </button>
-                        </div>
+            <div className='border border-gray-200 rounded-2xl p-10 mb-5'>
+                <p className='font-bold text-xl mb-1'>{name}</p>
+                <div className='flex justify-between'>
+                    <p className='text-base text-gray-400'>{expert}</p>
+                    <p className='text-base text-gray-400'>Appointment Fee: <span>{visitPrice} Taka</span></p>
+                </div>
+                <hr className='text-gray-500 border-dotted w-full my-3'/>
+                <button 
+                    onClick={()=>{
+                                handleRemoveBooking(lawyer)
+                            }}
+                    href="#_" className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold leading-6 text-red-700 hover:text-white border border-red-600 rounded-full hover:bg-red-500 focus:outline-none focus:ring-red-600 cursor-pointer">
+                    Cancel Appointment
+                </button>
+            </div>
             
         </div>
     );
--- src/Pages/LawyerDetails.jsx
+++ src/Pages/LawyerDetails.jsx
@@ -1,101 +1,113 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLoaderData, useParams} from 'react-router';
 import exclamatoryImage from'../assets/exclamatory.png';
 import { addBooking } from '../Utilities';
-
-
-
-
-
-
+import ErrorPage from './ErrorPage';
+import Button from '../Components/UI/Button';
 const LawyerDetails = () => {
     const LawyersData = useLoaderData();
-    
             // jei id er view details e click korchi sei id er value ta asbe 
     const {id} = useParams();
-    // console.log(id, typeof(id))
+            console.log(id, typeof(id))
             // jei id te click korchi sei er data khojar jonno find use korchi
     const singleLawyerData = LawyersData.find(lawyer => lawyer.id === parseInt(id));
- 
+    console.log(singleLawyerData)
             // singleLawyerData er data destructure korchi
     const {name, licenseNo, workingExperience,photo,expert, visitPrice} = singleLawyerData || {};
-
-        //ui er jonno: book e click korle lawyer er data kono ekjaigate rakhbo
-//    const [lawyerBooking, setLawyeBooking] = useState([])
+            //ui er jonno: book e click korle lawyer er data kono ekjaigate rakhbo
    const handleBooking = (singlerLawyer) =>{
     // setLawyeBooking([...lawyerBooking, singleLawyer])
     addBooking(singlerLawyer)
    }
-//    console.log(lawyerBooking)
+   const[idFound,setIdFound] = useState(false);
+   useEffect( ()=>{
+        if(singleLawyerData){
+            setIdFound(true)
+        }
+       else{ setIdFound(false)}
+   } , [singleLawyerData])
 
+//    const [lawyer] = useState(false);
     return (
         <div>
-            <div className='bg-gray-100 rounded-2xl p-7 text-center'>
-                <h2 className='text-3xl font-extrabold'>Lawyer’s Profile Details</h2>
-                <p className='tex-base mt-4 mb-8'>A lawyer, also known as an attorney, is a legal professional who provides advice, representation, and legal <br /> services to individuals, businesses, and organizations.</p>
-            </div>
-            <br />
-            <div className='p-7 border border-gray-300 rounded-2xl'>
-                <div className="flex flex-col sm:flex-row gap-5 md:gap-10 lg:gap-20 items-center">
-                    <div className='bg-gray-100 rounded-xl p-3 sm:p-6 sm:w-20% md:w-[30%] lg:w-[35%] flex justify-center items-center'>
-                        <img className='w-30 h-30 sm:w-30 sm:h-30  md:w-40 md:h-40 lg:w-60 lg:h-60 rounded-full object-cover' src={photo}  alt="Lawyer Photo" /> 
+            {
+                idFound === false?
+                (
+                    <div className='flex flex-col items-center space-y-5 p-2'>
+                        <h1 className='text-9xl text-red-700 font-extrabold mt-10 mb-5'>404</h1>
+                        <p>Oops! The page You are looking for doesn't exist.</p>
+                        <Link to='/'><Button label='Go Back Home'></Button></Link>
                     </div>
-                    <div className='w-[75%] space-y-2'>   
-                    <p className='inline-block py-1 px-3 rounded-3xl text-sm bg-blue-100 text-blue-900'>  {workingExperience}</p>
-                    <h2 className="text-xl md:text-3xl font-extrabold">{name}</h2>
-                    <div className='flex flex-col sm:flex-row sm:gap-10 sm:items-center'>
-                        <p className='text-base text-gray-400'>{expert}</p>
-                        <p className='text-base flex gap-2  text-gray-500 my-1'> <span>&#174;</span>  License No: {licenseNo}</p>
+                )
+                :
+                <div>
+                    <div className='bg-gray-100 rounded-2xl p-7 text-center'>
+                        <h2 className='text-3xl font-extrabold'>Lawyer’s Profile Details</h2>
+                        <p className='tex-base mt-4 mb-8'>A lawyer, also known as an attorney, is a legal professional who provides advice, representation, and legal <br /> services to individuals, businesses, and organizations.</p>
                     </div>
-                    <div className='flex flex-col sm:flex-row sm:gap-2'>
-                        <p className='font-bold'>Availability</p>
-                        <div className='flex'>
-                            <p className=' py-1 px-3 rounded-3xl text-sm bg-amber-50 text-amber-500 cursor-pointer'>  Saturday </p>
-                            <p className=' py-1 px-3 rounded-3xl text-sm bg-amber-50 text-amber-500 cursor-pointer'>  Sunday </p>
-                            <p className=' py-1 px-3 rounded-3xl text-sm bg-amber-50 text-amber-500 cursor-pointer'>  Monday </p>
+                    <br />
+                    <div className='p-7 border border-gray-300 rounded-2xl'>
+                        <div className="flex flex-col sm:flex-row gap-5 md:gap-10 lg:gap-20 items-center">
+                            <div className='bg-gray-100 rounded-xl p-3 sm:p-6 sm:w-20% md:w-[30%] lg:w-[35%] flex justify-center items-center'>
+                                <img className='w-30 h-30 sm:w-30 sm:h-30  md:w-40 md:h-40 lg:w-60 lg:h-60 rounded-full object-cover' src={photo}  alt="Lawyer Photo" /> 
+                            </div>
+                            <div className='w-[75%] space-y-2'>   
+                            <p className='inline-block py-1 px-3 rounded-3xl text-sm bg-blue-100 text-blue-900'>  {workingExperience}</p>
+                            <h2 className="text-xl md:text-3xl font-extrabold">{name}</h2>
+                            <div className='flex flex-col sm:flex-row sm:gap-10 sm:items-center'>
+                                <p className='text-base text-gray-400'>{expert}</p>
+                                <p className='text-base flex gap-2  text-gray-500 my-1'> <span>&#174;</span>  License No: {licenseNo}</p>
+                            </div>
+                            <div className='flex flex-col sm:flex-row sm:gap-2'>
+                                <p className='font-bold'>Availability</p>
+                                <div className='flex'>
+                                    <p className=' py-1 px-3 rounded-3xl text-sm bg-amber-50 text-amber-500 cursor-pointer'>  Saturday </p>
+                                    <p className=' py-1 px-3 rounded-3xl text-sm bg-amber-50 text-amber-500 cursor-pointer'>  Sunday </p>
+                                    <p className=' py-1 px-3 rounded-3xl text-sm bg-amber-50 text-amber-500 cursor-pointer'>  Monday </p>
+                                </div>
+
+                            </div>
+                            <div className='flex gap-2'>
+                                <p className='font-bold'>Consultation Fee:</p>
+                                        {/* taka. 400 dewa ache visitPrice e: tai age split kore taka lekha bad dilam */}
+                                <p className='text-green-800 font-bold'>Taka: {visitPrice}</p>
+                            </div>
+
+                            </div>
+                        
                         </div>
-
                     </div>
-                    <div className='flex gap-2'>
-                        <p className='font-bold'>Consultation Fee:</p>
-                                {/* taka. 400 dewa ache visitPrice e: tai age split kore taka lekha bad dilam */}
-                        <p className='text-green-800 font-bold'>Taka: {visitPrice}</p>
+                    <br />
+                    <div className='p-7 border border-gray-300 rounded-2xl'>
+                        <h2 className='text-xl font-extrabold text-center'>Book an Appointment</h2>
+                        <hr className='text-gray-500 border-dotted w-full my-3'/>
+                        <div className='flex justify-between items-center'>
+                            <p className='font-bold'>Availability</p>
+                            <p className=' py-1 px-3 border border-green-300 rounded-3xl text-sm font-bold bg-green-100 text-green-700 cursor-pointer'>  Lawyer Available Today </p>
+                        </div>
+                        <hr className='text-gray-500 border w-full my-3'/>
+                        <div className='inline-flex gap-3 py-1 px-3 rounded-3xl text-sm bg-red-50 text-amber-400 cursor-pointer mb-2'>
+                            <img src={exclamatoryImage} className='w-5 h-5' alt="" /> 
+                            <p className=''> 
+                                Due to high patient volume, we are currently accepting appointments for today only. We appreciate your understanding and cooperation. </p>
+                        </div>
+                        <Link to='/booking'>
+                        <button 
+                        
+                            onClick={ ()=>{
+                                handleBooking(singleLawyerData)
+                            }
+                            }
+
+                            href="#_" className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold leading-6 text-white bg-green-600 border border-transparent rounded-full hover:bg-green-500 focus:outline-none focus:ring-green-600 cursor-pointer">
+                            Book Appointment Now
+                        </button>
+                        </Link>
                     </div>
-
-                    </div>
-                
                 </div>
-            </div>
-            <br />
-            <div className='p-7 border border-gray-300 rounded-2xl'>
-                <h2 className='text-xl font-extrabold text-center'>Book an Appointment</h2>
-                <hr className='text-gray-500 border-dotted w-full my-3'/>
-                <div className='flex justify-between items-center'>
-                    <p className='font-bold'>Availability</p>
-                    <p className=' py-1 px-3 border border-green-300 rounded-3xl text-sm font-bold bg-green-100 text-green-700 cursor-pointer'>  Lawyer Available Today </p>
-                </div>
-                <hr className='text-gray-500 border w-full my-3'/>
-                <div className='inline-flex gap-3 py-1 px-3 rounded-3xl text-sm bg-red-50 text-amber-400 cursor-pointer mb-2'>
-                    <img src={exclamatoryImage} className='w-5 h-5' alt="" /> 
-                    <p className=''> 
-                        Due to high patient volume, we are currently accepting appointments for today only. We appreciate your understanding and cooperation. </p>
-                </div>
-                <Link to='/booking'>
-                <button 
-                
-                    onClick={ ()=>{
-                        handleBooking(singleLawyerData)
-                    }
-                    }
-
-                    href="#_" className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold leading-6 text-white bg-green-600 border border-transparent rounded-full hover:bg-green-500 focus:outline-none focus:ring-green-600 cursor-pointer">
-                    Book Appointment Now
-                </button>
-                </Link>
-                
-
-            </div>
+            }
         </div>
+        
     );
 };
 
